05651759a107d17f2cec6f3ab060d7144dbdf13d
jeevanjames2000 <jeevanjames2000@gmail.com>
2024-07-11 16:18:42 +0530

    available slots,occupied slots

--- controllers/gym_booking_controller.js
+++ controllers/gym_booking_controller.js
@@ -2,7 +2,6 @@ const sql = require("mssql");
 const Gym_Master = require("../models/gym_Master");
 const Gym_Booked_Slots = require("../models/gym_booked_slots");
 const qr = require("qrcode");
-
 const convertTime = (time) => {
   let [hours, minutes, period] = time.match(/(\d+):(\d+) (\w+)/).slice(1);
   const originalTime = `${hours}:${minutes} ${period}`;
@@ -10,7 +9,6 @@ const convertTime = (time) => {
   if (period === "AM" && hours === "12") hours = "00";
   return { formattedTime: `${hours}:${minutes}:00`, originalTime };
 };
-
 const generateQRCode = async (data) => {
   try {
     const qrCodeDataUri = await qr.toDataURL(JSON.stringify(data));
@@ -20,7 +18,6 @@ const generateQRCode = async (data) => {
     throw new Error("Error generating QR code");
   }
 };
-
 module.exports = {
   insertGymMasterScheduling: async function (req, res) {
     try {
@@ -114,60 +111,19 @@ module.exports = {
       res.status(500).json({ error: "Failed to insert gym scheduling record" });
     }
   },
-
-  // getGymSchedulesByLocation: async function (req, res) {
-  //   const locationId = req.params.locationId;
-  //   const date = req.params.date;
-  //   try {
-  //     const pool = req.app.locals.sql;
-  //     const result = await pool
-  //       .request()
-  //       .input("Location", sql.VarChar, locationId)
-  //       .input("Date", sql.Date, date)
-  //       .query(
-  //         "SELECT * FROM GYM_SCHEDULING_MASTER WHERE Location = @Location AND start_date = @Date"
-  //       );
-  //     if (result.recordset.length === 0) {
-  //       return res.status(404).json({
-  //         message: "No gym schedules found for the specified location and date",
-  //       });
-  //     }
-
-  //     const data = result.recordset;
-  //     const filteredData = data.map((item) => ({
-  //       start_date: item.start_date,
-  //       start_time: item.start_time,
-  //       end_time: item.end_time,
-  //       location: item.Location,
-  //       available: item.max_count - item.occupied,
-  //       occupied: item.occupied,
-  //     }));
-  //     res.status(200).json({ fullData: data, filteredData: filteredData });
-  //   } catch (err) {
-  //     console.error(
-  //       "Error fetching gym schedules by location ID and date:",
-  //       err
-  //     );
-  //     res.status(500).json({ error: "Failed to fetch gym schedules" });
-  //   }
-  // },
-
   getGymSchedulesByLocationMongo: async function (req, res) {
     const locationId = req.params.locationId;
     const date = new Date(req.params.date);
-
     try {
       const result = await Gym_Master.find({
         Location: locationId,
         start_date: date,
       });
-
       if (result.length === 0) {
         return res.status(404).json({
           message: "No gym schedules found for the specified location and date",
         });
       }
-
       res.status(200).json(result);
     } catch (err) {
       console.error(
@@ -187,7 +143,6 @@ module.exports = {
       Location,
       campus,
     } = req.body;
-
     if (
       !regdNo ||
       !start_date ||
@@ -201,47 +156,55 @@ module.exports = {
         .status(400)
         .json({ status: "error", message: "Missing required fields" });
     }
-
     try {
-      // Check if user already has active slots
       const activeSlots = await Gym_Booked_Slots.find({
         regdNo,
         status: "booked",
       });
       if (activeSlots.length > 0) {
-        return res
-          .status(400)
-          .json({ status: "error", message: "User already has active slots." });
+        return res.status(400).json({
+          status: "error",
+          message: "User already have active slots.",
+        });
       }
-
+      const availableSlots = await Gym_Master.find({
+        Gym_scheduling_id,
+      });
+      const matchingSlots = availableSlots.filter((slot) => {
+        return (
+          slot.start_date.toISOString().split("T")[0] === start_date &&
+          slot.start_time === start_time
+        );
+      });
+      const availableSlot = matchingSlots.find((slot) => slot.available > 0);
+      if (!availableSlot) {
+        return res.status(400).json({
+          status: "error",
+          message: "No available slots.",
+        });
+      }
       const currentDate = new Date(start_date);
-
       const convertTime = (time) => {
         const [formattedTime, modifier] = time.split(" ");
         let [hours, minutes] = formattedTime.split(":");
         hours = parseInt(hours);
         minutes = parseInt(minutes);
-
         if (modifier === "PM" && hours !== 12) {
           hours += 12;
         } else if (modifier === "AM" && hours === 12) {
           hours = 0;
         }
-
         return { formattedTime: `${hours}:${minutes}`, originalTime: time };
       };
-
       const {
         formattedTime: formattedStartTime,
         originalTime: originalStartTime,
       } = convertTime(start_time);
       const { formattedTime: formattedEndTime, originalTime: originalEndTime } =
         convertTime(end_time);
-
       for (let i = 0; i < 31; i++) {
         const startDate = new Date(currentDate);
         startDate.setDate(currentDate.getDate() + i);
-
         const bookingData = {
           regdNo,
           start_date: startDate.toISOString().split("T")[0],
@@ -250,9 +213,7 @@ module.exports = {
           Location,
           campus,
         };
-
         const qrCode = await generateQRCode(bookingData);
-
         const booking = new Gym_Booked_Slots({
           regdNo,
           Gym_scheduling_id,
@@ -268,16 +229,21 @@ module.exports = {
           campus,
           qr_code: qrCode,
         });
-
         await booking.save();
       }
-
+      await Gym_Master.updateOne(
+        { _id: availableSlot._id },
+        { $inc: { available: -1 } }
+      );
+      await Gym_Master.updateOne(
+        { _id: availableSlot._id },
+        { $inc: { occupied: +1 } }
+      );
       return res.status(200).json({
         status: "success",
         message: "Slots booked successfully for the next 30 days.",
       });
     } catch (error) {
-      console.error("Error booking slots:", error);
       return res
         .status(500)
         .json({ status: "error", message: "Internal server error" });
@@ -285,54 +251,59 @@ module.exports = {
   },
   getGymBookingsByRegdNo: async (req, res) => {
     const { regdNo } = req.params;
-
     if (!regdNo) {
       return res.status(400).send("Missing required parameter: regdNo");
     }
-
     try {
       const bookings = await Gym_Booked_Slots.find({ regdNo });
-
       if (bookings.length === 0) {
         return res
           .status(404)
           .send("No bookings found for the provided regdNo");
       }
-
       res.status(200).json(bookings);
     } catch (error) {
       console.error("Error fetching bookings:", error);
       res.status(500).send("Internal server error");
     }
   },
-
-  // delete api
-
   deleteGymBookingsByRegdNo: async (req, res) => {
     const { regdNo } = req.params;
-
     if (!regdNo) {
       return res.status(400).send("Missing required parameter: regdNo");
     }
-
     try {
-      const result = await Gym_Booked_Slots.deleteMany({ regdNo });
-
-      if (result.deletedCount === 0) {
+      const bookings = await Gym_Booked_Slots.find({ regdNo });
+      if (bookings.length === 0) {
         return res
           .status(404)
           .send("No bookings found for the provided regdNo");
       }
-
+      const result = await Gym_Booked_Slots.deleteMany({ regdNo });
+      const updates = bookings.map(async (booking) => {
+        const { Gym_scheduling_id, start_date, start_time } = booking;
+        const matchingSlot = await Gym_Master.findOne({
+          Gym_scheduling_id,
+          start_date: start_date.toISOString().split("T")[0],
+          start_time,
+        });
+        if (matchingSlot) {
+          await Gym_Master.updateOne(
+            { _id: matchingSlot._id },
+            { $inc: { available: +1 } }
+          );
+          await Gym_Master.updateOne(
+            { _id: matchingSlot._id },
+            { $inc: { occupied: -1 } }
+          );
+        }
+      });
+      await Promise.all(updates);
       res.status(200).json({ message: "Bookings deleted successfully" });
     } catch (error) {
-      console.error("Error deleting bookings:", error);
       res.status(500).send("Internal server error");
     }
   },
-
-  // update api
-
   updateGymMasterSchedulingMongo: async function (req, res) {
     const {
       regdNo,
@@ -343,7 +314,6 @@ module.exports = {
       Location,
       campus,
     } = req.body;
-
     if (
       !regdNo ||
       !start_date ||
@@ -357,44 +327,35 @@ module.exports = {
         .status(400)
         .json({ status: "error", message: "Missing required fields" });
     }
-
     try {
       const existingSlot = await Gym_Booked_Slots.findOne({
         regdNo,
         Gym_scheduling_id,
       });
-
       if (!existingSlot) {
         return res
           .status(404)
           .json({ status: "error", message: "Slot not found" });
       }
-
       const currentDate = new Date(start_date);
-
       const convertTime = (time) => {
         const [formattedTime, modifier] = time.split(" ");
         let [hours, minutes] = formattedTime.split(":");
         hours = parseInt(hours);
         minutes = parseInt(minutes);
-
         if (modifier === "PM" && hours !== 12) {
           hours += 12;
         } else if (modifier === "AM" && hours === 12) {
           hours = 0;
         }
-
         return { formattedTime: `${hours}:${minutes}`, originalTime: time };
       };
-
       const {
         formattedTime: formattedStartTime,
         originalTime: originalStartTime,
       } = convertTime(start_time);
       const { formattedTime: formattedEndTime, originalTime: originalEndTime } =
         convertTime(end_time);
-
-      // Update the existing slot with new data
       existingSlot.start_date = currentDate;
       existingSlot.start_time = start_time;
       existingSlot.end_time = end_time;
@@ -402,7 +363,6 @@ module.exports = {
       existingSlot.campus = campus;
       existingSlot.generated_date = new Date();
       existingSlot.generated_time = new Date().toISOString();
-
       const bookingData = {
         regdNo,
         start_date: currentDate.toISOString().split("T")[0],
@@ -411,12 +371,9 @@ module.exports = {
         Location,
         campus,
       };
-
       const qrCode = await generateQRCode(bookingData);
       existingSlot.qr_code = qrCode;
-
       await existingSlot.save();
-
       return res.status(200).json({
         status: "success",
         message: "Slot updated successfully.",
